cfd88df68e2638b4aaac9bad6986118c449aba42
Bill Indest <56596420+awindest@users.noreply.github.com>
2024-04-03 19:55:00 -0400

    adding images

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.png"]),
-	mimeTypes: {".png":"image/png"},
+	assets: new Set(["favicon.png","pockethost.avif"]),
+	mimeTypes: {".png":"image/png",".avif":"image/avif"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.a6b68382.js","app":"_app/immutable/entry/app.3bec2dd8.js","imports":["_app/immutable/entry/start.a6b68382.js","_app/immutable/chunks/scheduler.d3678df4.js","_app/immutable/chunks/singletons.b2cfb3a0.js","_app/immutable/chunks/control.f5b05b5f.js","_app/immutable/entry/app.3bec2dd8.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.d3678df4.js","_app/immutable/chunks/index.d932291b.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.289e1c00.js","app":"_app/immutable/entry/app.22080bc0.js","imports":["_app/immutable/entry/start.289e1c00.js","_app/immutable/chunks/scheduler.d3678df4.js","_app/immutable/chunks/singletons.83826954.js","_app/immutable/chunks/control.f5b05b5f.js","_app/immutable/entry/app.22080bc0.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.d3678df4.js","_app/immutable/chunks/index.d932291b.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
---
+++ .vercel/output/static/_app/immutable/chunks/pockethost-io.efc1cda6.js
@@ -0,0 +1,35 @@
+import{s as G,e as I,j as R,n as Y}from"./scheduler.d3678df4.js";import{S as J,i as K,r as N,u as O,v as Q,d as V,t as X,w as Z,g as d,s as u,H as ee,h as m,D as f,c as h,G as te,k as j,a as n,f as o}from"./index.d932291b.js";import{g as ae,a as U}from"./spread.8a54911c.js";import{M as se}from"./mdsvex.b1a57069.js";function ne(w){let t,r='<img width="100%" src="pockethost.avif" title="PocketHost.io logo from web site"/>',i,l,a="Introduction",c,p,A="Found on reddit:",C,b,E=`<pre class="shiki poimandres" style="background-color: #1b1e28" tabindex="0"><code><span class="line"><span style="color: #a6accd">Hello, creator of PocketHost here. Someone from the</span></span>
+<span class="line"><span style="color: #a6accd">Discord server shared this link.</span></span>
+<span class="line"><span style="color: #a6accd"></span></span>
+<span class="line"><span style="color: #a6accd">PocketHost was created to support my own projects, and</span></span>
+<span class="line"><span style="color: #a6accd">other people found it useful. As of this writing,</span></span>
+<span class="line"><span style="color: #a6accd">there are over 3,000 users and ~~4,000~~ 5,000 instances</span></span>
+<span class="line"><span style="color: #a6accd">of PocketBase running.</span></span>
+<span class="line"><span style="color: #a6accd"></span></span>
+<span class="line"><span style="color: #a6accd">It&#39;s free because I pay for it and don&#39;t</span></span>
+<span class="line"><span style="color: #a6accd">charge anyone :)</span></span>
+<span class="line"><span style="color: #a6accd"></span></span>
+<span class="line"><span style="color: #a6accd">Maybe some day there will be a paid tier, but for now,</span></span>
+<span class="line"><span style="color: #a6accd">please enjoy PocketHost without restrictions of any kind.</span></span>
+<span class="line"><span style="color: #a6accd"></span></span>
+<span class="line"><span style="color: #a6accd">The paid tier that feels best to me is something like</span></span>
+<span class="line"><span style="color: #a6accd">$20/mo for unlimited instances, bandwidth, and storage.</span></span>
+<span class="line"><span style="color: #a6accd">Because of how PocketHost is used, who uses it, and my</span></span>
+<span class="line"><span style="color: #a6accd">personal values, a flat access fee makes more sense than</span></span>
+<span class="line"><span style="color: #a6accd">metering out every byte and CPU cycle. I don&#39;t think</span></span>
+<span class="line"><span style="color: #a6accd">PocketHost is a utility computing hosting provider.</span></span>
+<span class="line"><span style="color: #a6accd">It&#39;s a specialized hosting provider to make an awesome</span></span>
+<span class="line"><span style="color: #a6accd">user experience for devs and makers looking to maintain</span></span>
+<span class="line"><span style="color: #a6accd">sovereignty while getting up and running quickly.</span></span>
+<span class="line"><span style="color: #a6accd"></span></span>
+<span class="line"><span style="color: #a6accd"></span></span></code></pre>`,H,y,S="Reaction",P,_,B=`Wow! What is going on? I dug a little deeper an found this
+person is the real deal. He may make money later but right now
+the hot PocketBase database is free… and easy to use.`,M,k,q=`You can explore more at
+<a href="https://pockethost.io" rel="nofollow">https://pockethost.io</a>`,T,g,D=`So how does it work? First you create an account at
+PocketHost.io and then create a pocketbase collection. Mine
+is called <code>posts</code>. Then you read the docs and figure out that
+you access the database via a REST API that is simple to use.`,$,v,z="Deployment",L,x,F=`Using <a href="https://vercel.com" rel="nofollow"><code>vercel</code></a> makes deployment so much easier. After testing
+your code you commit it to github and if you have linked your
+github project to <code>vercel</code> it is automatically deployed and available
+world-wide on either <code>&lt;your domain&gt;.vercel.app</code> or your
+custom domain. Pretty cool.`;return{c(){t=d("p"),t.innerHTML=r,i=u(),l=d("h3"),l.textContent=a,c=u(),p=d("p"),p.textContent=A,C=u(),b=new ee(!1),H=u(),y=d("h3"),y.textContent=S,P=u(),_=d("p"),_.textContent=B,M=u(),k=d("p"),k.innerHTML=q,T=u(),g=d("p"),g.innerHTML=D,$=u(),v=d("h3"),v.textContent=z,L=u(),x=d("p"),x.innerHTML=F,this.h()},l(e){t=m(e,"P",{align:!0,"data-svelte-h":!0}),f(t)!=="svelte-il8ht4"&&(t.innerHTML=r),i=h(e),l=m(e,"H3",{id:!0,"data-svelte-h":!0}),f(l)!=="svelte-1h3clxg"&&(l.textContent=a),c=h(e),p=m(e,"P",{"data-svelte-h":!0}),f(p)!=="svelte-n7jb3d"&&(p.textContent=A),C=h(e),b=te(e,!1),H=h(e),y=m(e,"H3",{id:!0,"data-svelte-h":!0}),f(y)!=="svelte-881kco"&&(y.textContent=S),P=h(e),_=m(e,"P",{"data-svelte-h":!0}),f(_)!=="svelte-j76usi"&&(_.textContent=B),M=h(e),k=m(e,"P",{"data-svelte-h":!0}),f(k)!=="svelte-2cwo9z"&&(k.innerHTML=q),T=h(e),g=m(e,"P",{"data-svelte-h":!0}),f(g)!=="svelte-15qc0k3"&&(g.innerHTML=D),$=h(e),v=m(e,"H3",{id:!0,"data-svelte-h":!0}),f(v)!=="svelte-vm528w"&&(v.textContent=z),L=h(e),x=m(e,"P",{"data-svelte-h":!0}),f(x)!=="svelte-13vkhg"&&(x.innerHTML=F),this.h()},h(){j(t,"align","center"),j(l,"id","introduction"),b.a=H,j(y,"id","reaction"),j(v,"id","deployment")},m(e,s){n(e,t,s),n(e,i,s),n(e,l,s),n(e,c,s),n(e,p,s),n(e,C,s),b.m(E,e,s),n(e,H,s),n(e,y,s),n(e,P,s),n(e,_,s),n(e,M,s),n(e,k,s),n(e,T,s),n(e,g,s),n(e,$,s),n(e,v,s),n(e,L,s),n(e,x,s)},p:Y,d(e){e&&(o(t),o(i),o(l),o(c),o(p),o(C),b.d(),o(H),o(y),o(P),o(_),o(M),o(k),o(T),o(g),o($),o(v),o(L),o(x))}}}function oe(w){let t,r;const i=[w[0],W];let l={$$slots:{default:[ne]},$$scope:{ctx:w}};for(let a=0;a<i.length;a+=1)l=I(l,i[a]);return t=new se({props:l}),{c(){N(t.$$.fragment)},l(a){O(t.$$.fragment,a)},m(a,c){Q(t,a,c),r=!0},p(a,[c]){const p=c&1?ae(i,[c&1&&U(a[0]),c&0&&U(W)]):{};c&2&&(p.$$scope={dirty:c,ctx:a}),t.$set(p)},i(a){r||(V(t.$$.fragment,a),r=!0)},o(a){X(t.$$.fragment,a),r=!1},d(a){Z(t,a)}}}const W={title:"A Story about PocketHost.io",description:"An interesting story about Ben Allfree, the person behind pockethost.io, a free pocketbase (as of today) cloud service.",date:"2023-12-10",categories:["pocketbase","svelte"],published:!0};function le(w,t,r){return w.$$set=i=>{r(0,t=I(I({},t),R(i)))},t=R(t),[t]}class de extends J{constructor(t){super(),K(this,t,le,oe,G,{})}}export{de as default,W as metadata};
