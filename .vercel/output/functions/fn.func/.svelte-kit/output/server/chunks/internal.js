d4b9b9f568e319a0d05e7fec1bbd9ceeb73994a5
awindest <william.indest@gmail.com>
2024-07-08 08:36:10 -0400

    added syncthing

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
@@ -191,7 +191,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "kyk8tz"
+  version_hash: "1p90v75"
 };
 function get_hooks() {
   return {};
